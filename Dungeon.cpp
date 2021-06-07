f44caee673c9afa57db73fe9f0820ed38c35eb8a
Andrew Baker <andrew_j_baker2@hotmail.com>
2021-06-07 19:23:13 +0100

    handle_loot_actions() and handle_room_with_chest() functions

--- Dungeon.cpp
+++ Dungeon.cpp
@@ -1,6 +1,7 @@
 // NOLINT(legal/copyright)
 #include <iostream>  // cin, cout, endl
 #include <string>
+#include <vector>
 
 #include "Dungeon.h"
 #include "Player.h"
@@ -13,6 +14,45 @@ void Dungeon::print_actions(int num_actions, std::string actions[]) {
   std::cout << "Choose an action:" << std::endl;
   for (int i = 0; i < num_actions; ++i) {
     std::cout << actions[i] << std::endl;
+  }
+}
+
+void Dungeon::handle_loot_actions(Room *room) {
+  std::cout << "You open the chest and find:" << std::endl;
+  for (std::vector<Item>::size_type i = 0; i < room->items.size(); ++i) {
+    std::cout << "  " << room->items[i].name << "" << std::endl;
+  }
+  std::cout << "Your health is now " << player.current_health <<
+   ", your attack is now " << player.attack << ", and your defence is now " <<
+   player.defence << std::endl;
+
+  player.loot_room(room);
+  room->clear_loot();
+}
+
+void Dungeon::handle_room_with_chest(Room *room) {
+  std::cout << "You enter the room and see a large chest in the middle." <<
+   std::endl;
+
+  std::string actions[] = {
+    "a. Loot the chest",
+    "b. Move to another room"
+  };
+
+  while (true) {
+    print_actions(2, actions);
+
+    std::string input;
+
+    std::cin >> input;
+    if (input == "a") {
+      handle_loot_actions(room);
+      return;
+    } else if (input == "b") {
+      return;
+    } else {
+      std::cout << "Incorrect choice." << std::endl;
+    }
   }
 }
 
@@ -39,7 +79,7 @@ void Dungeon::enter_room(Room *room) {
   if (room->enemies.size() != 0) {
     // handle room with enemy
   } else if (room->items.size() != 0) {
-    // handle room with chest
+    handle_room_with_chest(room);
   } else {
     handle_empty_room(room);
   }
