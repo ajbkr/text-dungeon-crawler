895aee3336ae20e37497bd8c58cca7cb4d4b114c
Andrew Baker <andrew_j_baker2@hotmail.com>
2021-06-07 19:47:07 +0100

    Build main using CMake

--- lint.sh
+++ lint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cppcheck --language=c++ *.cpp *.h && cpplint *.cpp *.h
+cppcheck --language=c++ --quiet --std=c++11 *.cpp *.h && cpplint --filter=-build/include_subdir --quiet *.cpp *.h
--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 // Copyright 2021 Andrew J. Baker
 #include <iostream>  // cout, endl
 
+#include "config.h"  // main_VERSION_MAJOR, main_VERSION_MINOR
 #include "Player.h"
 
 int main() {
+  std::cout << "Version " << main_VERSION_MAJOR << "." <<
+   main_VERSION_MINOR << std::endl;
+
   Player *player = new Player("Andrew", 6, 6, 6);
 
   std::cout << "Name: " << player->name << std::endl;
